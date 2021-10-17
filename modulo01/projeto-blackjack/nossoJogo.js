3a639b6a3aff07e34f76b5dd03e70b91827c4ae9
Alessandra-Ferreira <alessandra.ferreira0103@gmail.com>
2021-10-16 21:25:55 -0300

    Projeto de Fixação: Blackjack-Pequenas alterações

--- modulo01/projeto-blackjack/nossoJogo.js
+++ modulo01/projeto-blackjack/nossoJogo.js
@@ -10,12 +10,12 @@
  * 
  * 
  */
-console.log("Boas vindas ao jogo de Blackjack!")
+console.log("Bem vindo(a) ao jogo de Blackjack!")
 
 const comecarJogo = confirm("Quer iniciar uma nova rodada")
 
-if (comecarJogo) {
-   const sortearCartaUsuario1 = comprarCarta()
+   if (comecarJogo) {
+  const sortearCartaUsuario1 = comprarCarta()
   const sortearCartaUsuario2 = comprarCarta()
   const sortearCartaComputador1 = comprarCarta()
   const sortearCartaComputador2 = comprarCarta()
@@ -26,16 +26,13 @@ if (comecarJogo) {
   console.log(`Usuário - cartas: ${sortearCartaUsuario1.texto} ${sortearCartaUsuario2.texto} - ${pontosDoUsuario}`)
   console.log(`Computador - cartas: ${sortearCartaComputador1.texto} ${sortearCartaComputador2.texto} - ${pontosDoComputador}`)
 
-
-
-
 if (pontosDoUsuario > pontosDoComputador) {
-   console.log("Parabéns, você ganhou!!!")
+   console.log("O usuário ganhou!!!")
 } else if (pontosDoComputador > pontosDoUsuario) {
-   console.log('O computador ganhou! Quem sabe na proxima rodada!')
+   console.log('O computador ganhou!')
  } else if (pontosDoUsuario === pontosDoComputador) {
    console.log('Empate!')
  } 
  } else {
-   console.log('Fim do jogo') }
+   console.log('O Jogo acabou') }
  
